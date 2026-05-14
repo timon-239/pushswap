794bf29f0930331a196fc9e2837273020d564188
Timon Reis <timonreis@gmx.at>
2026-05-14 15:07:28 +0200

    swapping operations finished

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: tireis <tireis@student.42vienna.com>      #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/14 14:34:02 by tireis           #+#    #+#              */
-/*   Updated: 2026/05/14 15:01:25 by tireis          ###   ########.fr        */
+/*   Updated: 2026/05/14 15:07:04 by tireis          ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -25,4 +25,23 @@ void	reverse_ro_logic(stack_t **stack)
 	(*stack)->prev = last;
 	last->prev = NULL;
 	*stack = last;
+}
+
+void	rra(stack_t **a)
+{
+	reverse_ro_logic(stack_t * *a);
+	write(1, "rra\n"), 4);
+}
+
+void	rrb(stack_t **b)
+{
+	reverse_ro_logic(stack_t * *b);
+	write(1, "rrb\n"), 4);
+}
+
+void	rrr(stack_t **a, stack_t **b)
+{
+	reverse_ro_logic(stack_t * *a);
+	reverse_ro_logic(stack_t * *b);
+	write(1, "rrr\n"), 4);
 }
