19c4233dbd9cb26cb2ff3662f70c52f8beb32857
Timon Reis <timonreis@gmx.at>
2026-05-18 13:59:19 +0200

    chagnes

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tireis <tireis@student.42vienna.com>      #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/13 16:03:41 by tireis           #+#    #+#              */
-/*   Updated: 2026/05/14 12:43:47 by tireis          ###   ########.fr        */
+/*   Updated: 2026/05/18 13:59:11 by tireis          ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,21 @@ t_stack	*ft_stacklast(t_stack *stack);
 t_stack *ft_stacknew(int value);
 void	ft_stack_sa(t_stack **stack);
 void	ft_freestack(t_stack **stack);
+void	ft_swap_logic(t_stack **stack);
+void	sa(t_stack **stack_a);
+void	sb(t_stack **stack_b);
+void	ss(t_stack **stack_a, t_stack **stack_b);
+void	ft_push_logic(t_stack **dst, t_stack **src);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+void	reverse_ro_logic(t_stack **stack);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
+void	ft_rotate_logic(t_stack **stack);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+
 
 
 
