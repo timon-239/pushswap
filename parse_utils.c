5c29ca539e03db48c9eb5f4ea3ebf31a8e7b2735
Elias Bouallagui <eboualla@c2r1p4.42vienna.com>
2026-05-29 15:41:35 +0200

    duplicate checker fix

--- parse_utils.c
+++ parse_utils.c
@@ -33,22 +33,53 @@ int	is_num(char *str)
 int	has_duplicates(t_stack *stack)
 {
 	t_stack	*current;
-	int		last_value;
+	t_stack *temp;
 
 	if (stack == NULL || stack->next == NULL)
 		return (0);
 	current = stack;
 	while (current->next != NULL)
 	{
-		current = current->next;
-	}
-	last_value = current->value;
-	current = stack;
-	while (current->next != NULL)
-	{
-		if (current->value == last_value)
-			return (1);
+		temp = current->next;
+		while(temp != NULL)
+		{
+			if (temp->value == current->value)
+				return (1);
+			temp = temp->next;
+		}
 		current = current->next;
 	}
 	return (0);
 }
+/*
+#include <stdio.h>
+int main (void){
+	t_stack *node1;
+	t_stack *node2;
+	t_stack *node3;
+	t_stack *node4;
+	t_stack *node5;
+	t_stack *a;
+
+	node1 = ft_stacknew(9);
+	node2 = ft_stacknew(4);
+	node3 = ft_stacknew(0);
+	node4 = ft_stacknew(2);
+	node5 = ft_stacknew(0);
+
+	node1->next = node2;
+	node2->prev = node1;
+	node2->next = node3;
+	node3->prev = node2;
+	node3->next = node4;
+	node4->prev = node3;
+	node4->next = node5;
+	node5->prev = node4;
+	a = node1;
+	if(has_duplicates(a))
+		printf("hat duplicates");
+	else
+		printf("hat keine");
+	ft_freestack(&a);
+}
+*/
