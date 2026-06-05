883f2de8dd4ea2eb85d976821cbc449f5ad8b9e1
Elias Bouallagui <eboualla@c2r2p5.42vienna.com>
2026-06-05 14:21:23 +0200

    complex first try

---
+++ sort_complex.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_complex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eboualla <eboualla@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/05 13:07:10 by eboualla          #+#    #+#             */
+/*   Updated: 2026/06/05 14:20:41 by eboualla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int get_pivot(t_stack **a)
+{
+	
+}
+
+void sort_complex(t_stack **a, t_stack **b)
+{
+	int	i;
+	int j;
+	int size;
+	t_stack *pivot;
+
+	j = 0;
+	i = -1;
+	size = ft_stacksize(*a);
+	
+}
