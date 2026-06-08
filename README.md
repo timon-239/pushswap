71b5bd4303deb63753d299d7bda7c9c81e1f60d1
eboualla <eboualla@student.42vienna.com>
2026-06-08 15:10:29 +0200

    solved sort complex, added sources to readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 *THIS PROJECT BY EBOUALLA TIREIS.*
+
+sources:
+// https://www.geeksforgeeks.org/cpp/left-shift-right-shift-operators-c-cpp/
+//https://www.w3schools.com/c/c_bitwise_operators.php
+
--- sort_complex.c
+++ sort_complex.c
@@ -6,24 +6,46 @@
 /*   By: eboualla <eboualla@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 13:07:10 by eboualla          #+#    #+#             */
-/*   Updated: 2026/06/05 14:20:41 by eboualla         ###   ########.fr       */
+/*   Updated: 2026/06/08 15:09:52 by eboualla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_pivot(t_stack **a)
+#include <push_swap.h>
+
+int get_max_bits(int max_index)
 {
-	
+	int bits = 0;
+	while ((max_index >> bits) != 0)
+		bits++;
+	return (bits);
 }
 
 void sort_complex(t_stack **a, t_stack **b)
 {
-	int	i;
+	int i;
 	int j;
 	int size;
-	t_stack *pivot;
+	int max_index;
+	int max_bits;
 
-	j = 0;
-	i = -1;
-	size = ft_stacksize(*a);
-	
+	i = 0;
+	assign_index(*a);
+	max_index = get_max_index_value(*a);
+	max_bits = get_max_bits(max_index);
+	size = stack_size(*a);
+	while (i < max_bits)
+	{
+		j = 0;
+		while(j < size)
+		{
+			if ((((*a)->index >> i) & 1) == 0)
+				pb(a, b);
+			else
+				ra(a);
+			j++;
+		}
+		while(*b)
+			pa(a, b);
+	}
+	i++;
 }
